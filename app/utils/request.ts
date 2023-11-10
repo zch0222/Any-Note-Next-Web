520951f8356befb660ffe7b5ed0da746217d74a5
称霸幼儿园 <yxlmzch0222@gmail.com>
2023-11-10 14:51:50 +0800

    feat: 任务编辑次数统计图

--- app/utils/request.ts
+++ app/utils/request.ts
@@ -9,7 +9,7 @@ axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
 // 创建axios实例
 const service = axios.create({
     // axios中请求配置有baseURL选项，表示请求URL公共部分
-    baseURL: 'http://localhost:8089/',
+    baseURL: 'http://116.62.8.78:8089/',
     // 超时
     timeout: 30000
 })
