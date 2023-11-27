6b52685a273693fa0db88897a157c3fad7d87858
Cumulus <12094954+blackfiyre@user.noreply.gitee.com>
2023-11-27 18:19:13 +0800

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,21 @@
 'use client'
 
-import {Button, Card, FloatButton, List, Modal, notification, Select} from "antd";
+import {Button, Form, Input, List, message, Modal, notification, Select, Upload, UploadProps} from "antd";
 import './page.scss'
-import {EditOutlined, FileOutlined, FileTextOutlined, PlusOutlined} from "@ant-design/icons";
+import {ContainerOutlined, EditOutlined, FileOutlined, FileTextOutlined, PlusOutlined} from "@ant-design/icons";
 import {ColumnsType} from "antd/es/table";
 import type {Note} from "@/app/config/types";
 import {Book} from "@/app/config/types";
 import {useEffect, useState} from "react";
-import {addNote, getBooks, getNotesById, getPersonalBooks} from "@/app/api/note";
+import {addBook, addNote, getBooks, getNotesById, getPersonalBooks} from "@/app/api/note";
 import Link from "next/link";
 import {useRouter} from "next/navigation";
 import "../styles/globals.css"
 import Loading from "@/app/components/Loading";
 import {getAnnouncementsApi, redAnnouncementsApi} from "@/app/api/system";
 import FunctionButton from "@/app/components/FunctionButton";
+import TextArea from "antd/es/input/TextArea";
+import {ls} from "@/app/utils/storage";
 
 const columns: ColumnsType<Note> = [
     {
@@ -75,7 +77,7 @@ export default function Page() {
                 notification.open({
                     message: announcement.title,
                     description: <div dangerouslySetInnerHTML={{__html: announcement.content}}/>,
-                    placement:"top",
+                    placement: "top",
                     btn: <Button onClick={() => handleConfirm(announcement.id)}>确定</Button>,
                     onClose: () => handleConfirm(announcement.id)
                 });
@@ -123,15 +125,59 @@ export default function Page() {
 function FunctionGroup(props: any) {
 
     const router = useRouter();
+    const token = ls.get('accessToken');
     const [open, setOpen] = useState(false);
     const [initLoading, setInitLoading] = useState(false);
     const [loading, setLoading] = useState(false);
+    const [ addBookLoading, setAddBookLoading] = useState(false);
     const [listData, setListData] = useState<Note[]>(props.notesData);
+    const [bookFormOpen, setBookFormOpen] = useState(false);
+
+    const [bookForm, setBookForm] = useState({
+        name: "",
+        detail: "",
+        cover: "",
+        type: 0
+    })
+
+    const uploadProps: UploadProps = {
+        name: 'image',
+        action: 'http://116.62.8.78:8089/api/note/bases/covers',
+        headers: {
+            'accessToken': token
+        },
+        listType: "picture-card",
+        onChange(info) {
+            if (info.file.status !== 'uploading') {
+                console.log(info.file, info.fileList);
+            }
+            if (info.file.status === 'done') {
+                if (info.file.response.code == '00000') {
+                    console.log(info.file.response.data)
+                    setBookForm((prevState: any) => ({
+                        ...prevState,
+                        ["cover"]: info.file.response.data.url,
+                    }))
+                } else {
+                    message.error(info.file.response.msg)
+                }
+            } else if (info.file.status === 'error') {
+                message.error(`${info.file.name} 导入失败！`);
+            }
+        },
+    };
 
     const [bookData, setBookData] = useState<Book[]>([]);
     const [personalBookData, setPersonalBookData] = useState<Book[]>([])
 
-    console.log(props.notesData)
+    const normFile = (e: any) => {
+        if (Array.isArray(e)) {
+            return e;
+        }
+        return e?.fileList;
+    };
+
+
     const showModal = () => {
         getAllBooks()
         setOpen(true);
@@ -143,6 +189,15 @@ function FunctionGroup(props: any) {
 
     const onLoadMore = () => {
     };
+
+    const resetBookForm = () => {
+        setBookForm({
+            name: "",
+            detail: "",
+            cover: "",
+            type: 0
+        })
+    }
 
     const getAllBooks = async () => {
         const params = {
@@ -158,6 +213,14 @@ function FunctionGroup(props: any) {
         })
     }
 
+    const onBookFormChange = (e: any) => {
+        const {name, value} = e.target;
+        setBookForm((prevState: any) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    }
+
     const addNewNote = (id: any) => {
         const data = {
             title: '无标题笔记',
@@ -167,6 +230,17 @@ function FunctionGroup(props: any) {
             console.log(res)
             if (res.data.code == "00000") {
                 router.push('/components/MarkDownEdit/' + res.data.data);
+            }
+        })
+    }
+
+    const addNewBook = () => {
+        setAddBookLoading(true)
+        addBook(bookForm).then(res => {
+            setAddBookLoading(false)
+            if (res.data.code == "00000") {
+                message.success("新增知识库成功！")
+                router.push('/dashboard/bookDetail/' + res.data.data.id)
             }
         })
     }
@@ -188,7 +262,12 @@ function FunctionGroup(props: any) {
 
     return (
         <div className={"flex_middle"} style={{justifyContent: "start"}}>
-            <FunctionButton title={'新建笔记'} content={'文档'} clickEvent={showModal} icon={<EditOutlined style={{fontSize: 20}}/>}/>
+            <FunctionButton title={'新建笔记'} content={'文档'} clickEvent={showModal}
+                            icon={<EditOutlined style={{fontSize: 20}}/>}/>
+            <FunctionButton title={'新建知识库'} content={'知识库'} clickEvent={() => {
+                setBookFormOpen(true)
+            }}
+                            icon={<ContainerOutlined style={{fontSize: 20}}/>}/>
 
             <Modal
                 title="新建笔记"
@@ -200,19 +279,19 @@ function FunctionGroup(props: any) {
                 cancelText="取消"
             >
                 <div>选择一个知识库</div>
-                <h4>组织知识库</h4>
-                <List
-                    dataSource={bookData}
-                    renderItem={(item) => (
-                        <List.Item onClick={() => addNewNote(item.id)} className={'hover'}>
-                            <List.Item.Meta
-                                avatar={<FileTextOutlined style={{fontSize: 24}}/>}
-                                description={item.knowledgeBaseName}
-                            />
-                        </List.Item>
-                    )}
-                />
-                <h4>非组织知识库</h4>
+                {/*<h4>组织知识库</h4>*/}
+                {/*<List*/}
+                {/*    dataSource={bookData}*/}
+                {/*    renderItem={(item) => (*/}
+                {/*        <List.Item onClick={() => addNewNote(item.id)} className={'hover'}>*/}
+                {/*            <List.Item.Meta*/}
+                {/*                avatar={<FileTextOutlined style={{fontSize: 24}}/>}*/}
+                {/*                description={item.knowledgeBaseName}*/}
+                {/*            />*/}
+                {/*        </List.Item>*/}
+                {/*    )}*/}
+                {/*/>*/}
+                <h4>知识库</h4>
                 <List
                     dataSource={personalBookData}
                     renderItem={(item) => (
@@ -226,14 +305,45 @@ function FunctionGroup(props: any) {
                 />
             </Modal>
 
-            {/*<Card style={{border: '2px #01B96B solid'}} bodyStyle={{display: 'flex', padding: 10}} hoverable>*/}
-            {/*    <ContainerOutlined style={{fontSize: 20}}/>*/}
-
-            {/*    <div className={'function'}>*/}
-            {/*        <div className={'function_title'}>新建知识库</div>*/}
-            {/*        <div className={'function_content'}>文档、思维导图、视频</div>*/}
-            {/*    </div>*/}
-            {/*</Card>*/}
+            <Modal
+                title="新建知识库"
+                open={bookFormOpen}
+                confirmLoading={addBookLoading}
+                onOk={() => {
+                    setBookFormOpen(false)
+                    addNewBook()
+                }}
+                onCancel={() => {
+                    setBookFormOpen(false)
+                }}
+                afterOpenChange={resetBookForm}
+                // destroyOnClose={true}
+                // footer={null}
+                okText="确认"
+                cancelText="取消"
+            >
+                <Form
+                    labelCol={{span: 4}}
+                    wrapperCol={{span: 14}}
+                    layout="horizontal"
+                    style={{maxWidth: 600}}
+                >
+                    <Form.Item label="知识库名称">
+                        <Input name={"name"} value={bookForm.name} onChange={onBookFormChange}/>
+                    </Form.Item>
+                    <Form.Item label="知识库详情">
+                        <TextArea name={"detail"} value={bookForm.detail} onChange={onBookFormChange} rows={4}/>
+                    </Form.Item>
+                    <Form.Item label="封面" valuePropName="fileList" getValueFromEvent={normFile}>
+                        <Upload {...uploadProps}>
+                            <div>
+                                <PlusOutlined/>
+                                <div style={{marginTop: 8}}>Upload</div>
+                            </div>
+                        </Upload>
+                    </Form.Item>
+                </Form>
+            </Modal>
         </div>
     )
 }
